024e8036bae6a399739b3568683018c38b58e640
regvedpande <regregd@outlook.com>
2025-09-30 22:18:36 +0530

    Beginning of Enterprise Hospital Management System

---
+++ Hospital.Utilities/WebSiteRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public static class WebSiteRoles
+    {
+        public const string Website_Admin = "Website_Admin";
+        public const string Website_Patient = "Website_Patient";
+        public const string Website_Doctor = "Website_Doctor";
+
+    }
+
+}
---
+++ Hospital.ViewModels/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Hospital.ViewModels
+{
+    public class Class1
+    {
+
+    }
+}
