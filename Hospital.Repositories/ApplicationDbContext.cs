b934957b2dbfa565fa9be0360687e6f0120dc9ea
regvedpande <regregd@outlook.com>
2025-10-01 18:36:56 +0530

    Working with Administrative Right Features

--- Hospital.Repositories/ApplicationDbContext.cs
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hospital.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore; // Add this using directive
 
@@ -14,5 +15,6 @@ namespace Hospital.Repositories
         {
 
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } 
     }
 }
---
+++ Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,58 @@
+﻿using Hospital.Models;
+using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }               
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (!_roleManager.RoleExistsAsync(WebSiteRoles.Website_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Patient)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Doctor)).GetAwaiter().GetResult();
+                // If roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Regved",
+                    Email = "regregd@outlook.com"
+
+                },"regvedpande").GetAwaiter().GetResult();
+                var Appuser = _userManager.FindByEmailAsync("regregd@outlook.com").GetAwaiter().GetResult();
+                if (Appuser != null)
+                {
+                     _userManager.AddToRoleAsync(Appuser, WebSiteRoles.Website_Admin).GetAwaiter().GetResult();
+                }
+
+            }
+        }
+    }
+}
