be44a770c73fad8474069f317fbe1e5526fa7a3b
regvedpande <regregd@outlook.com>
2025-10-03 20:57:25 +0530

    Adding Models

---
+++ Hospital.Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string HospitalId { get; set; }
+        public string Hospital { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        
+    }
+}
---
+++ Hospital.Models/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public string Type { get; set; }
+        public int Status { get; set; }
+        public int HospitalId { get; set; }
+        public string Hospital { get; set; }
+
+    }
+}
