3199070c46d66e6f1844f6eb4c2db0098d65828c
regvedpande <regregd@outlook.com>
2025-10-04 19:56:45 +0530

    Creating Models

--- Hospital.Models/ApplicationUser.cs
+++ Hospital.Models/ApplicationUser.cs
@@ -10,6 +10,30 @@ namespace Hospital.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public string Nationality { get; set; }
 
+        public String Address { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public string Specialist { get; set; }
+
+        public string MyProperty { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+        public ICollection<Payroll> Payrolls { get; set; }
+
+
+    }
+}
+
+namespace Hospital.Models
+{
+    public enum Gender
+    {
+        Male, Female, Other
     }
 }
