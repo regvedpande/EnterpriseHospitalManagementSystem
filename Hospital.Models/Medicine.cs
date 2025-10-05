8d943d57315f76412c6ae2a2275d45430984c03d
regvedpande <regregd@outlook.com>
2025-10-05 15:35:32 +0530

    Additional Models

--- Hospital.Models/Medicine.cs
+++ Hospital.Models/Medicine.cs
@@ -14,6 +14,7 @@ namespace Hospital.Models
         public decimal Cost { get; set; }
         public string Description { get; set; }
         public ICollection<MedicineReport> MedicineReport { get; set; }
+        public ICollection<PrescribedMedicine> PrescribedMedicine { get; set; }
         public int MyProperty { get; set; }
 
     }
--- Hospital.Models/TestPrice.cs
+++ Hospital.Models/TestPrice.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Hospital.Models
+
 {
-    public class PrescribedMedicine
+    public class TestPrice
     {
         public int Id { get; set; }
-        public Medicine Medicine { get; set; }
-        public PatientReport PatientReport { get; set; }
-
+        public string TestCode { get; set; }
+        public decimal Price { get; set; }
+        public Lab Lab { get; set; }
+        public Bill Bill { get; set; }
 
     }
 }
